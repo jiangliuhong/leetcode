94d282d9b947491bac3ad2a298d1909fe8275ef5
jiangliuhong <jerome950523@gmail.com>
2020-05-21 23:50:11 +0800

    Update SolutionCoinChange.java

--- src/main/java/com/jarome/leetcode/test2/SolutionCoinChange.java
+++ src/main/java/com/jarome/leetcode/test2/SolutionCoinChange.java
@@ -6,45 +6,62 @@ import java.util.Map;
 public class SolutionCoinChange {
 
     public int coinChange(int[] coins, int amount) {
-        return ccRec(coins,amount,new HashMap<>());
+        // return ccRec(coins, amount, new HashMap<>());
+        return dp(coins, amount);
     }
 
-    //递归求解，超出时间限制
-    private int ccRec(int[] coins, int amount, Map<Integer,Integer> temp){
-        if(amount == 0){
+    // 递归求解，超出时间限制
+    private int ccRec(int[] coins, int amount, Map<Integer, Integer> temp) {
+        if (amount == 0) {
             return 0;
-        }else if(amount < 0){
+        } else if (amount < 0) {
             return -1;
         }
         // 备忘录
         Integer amountVal = temp.get(amount);
-        if(amountVal != null) {
+        if (amountVal != null) {
             return amountVal;
         }
         Integer res = null;
         for (int i = 0; i < coins.length; i++) {
             int coin = coins[i];
             // 递归求值
-            int ccRes = ccRec(coins,  amount - coin,temp);
+            int ccRes = ccRec(coins, amount - coin, temp);
             if (ccRes == -1) {
                 continue;
             }
-            if(res == null){
+            if (res == null) {
                 res = 1 + ccRes;
-            }else {
+            } else {
                 res = Math.min(res, 1 + ccRes);
             }
         }
-        res =  res ==null ? -1 : res;
-        temp.put(amount,res);
+        res = res == null ? -1 : res;
+        temp.put(amount, res);
         return res;
     }
 
-    public int dp(int[] coins,int amount){
-        int[] dp = new int[amount + 1];
-        for (int i = 0; i < dp.length; i++) {
-            
-
+    public int dp(int[] coins, int amount) {
+        Integer[] dp = new Integer[amount + 1];
+        // 设置第一位为0
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                int coin = coins[j];
+                int sub = i - coin;;
+                if (sub < 0 || dp[sub] == -1) {
+                    continue;
+                }
+                if (dp[i] == null) {
+                    dp[i] = 1 + dp[sub];
+                } else {
+                    dp[i] = Math.min(dp[i], 1 + dp[sub]);
+                }
+            }
+            // 如过结束循环后，仍然为空，设置为-1，标记为未找到
+            if (dp[i] == null) {
+                dp[i] = -1;
+            }
         }
         return dp[amount];
     }
@@ -52,9 +69,9 @@ public class SolutionCoinChange {
     public static void main(String[] args) {
         SolutionCoinChange sc = new SolutionCoinChange();
         int[] coins = {1, 2, 5};
-//        int[] coins = {2};
+        // int[] coins = {2};
         int amount = 11;
-//        int amount = 3;
+        // int amount = 3;
         int res = sc.coinChange(coins, amount);
         System.out.println(res);
     }
