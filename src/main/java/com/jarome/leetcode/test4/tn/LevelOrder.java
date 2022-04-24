a681dda61734a080d6600f6ab15a0f10bd7eae8e
jiangliuhong <ja_rome@163.com>
2022-04-24 19:05:47 +0800

    update

---
+++ src/main/java/com/jarome/leetcode/test4/tn/LevelOrder.java
@@ -0,0 +1,37 @@
+package com.jarome.leetcode.test4.tn;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 二叉树的层序遍历
+ */
+public class LevelOrder {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int num = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < num; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
