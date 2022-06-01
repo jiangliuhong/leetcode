6785326e9e123d5900f8ced3e53b864f1914b615
jiangliuhong <ja_rome@163.com>
2022-06-01 09:30:06 +0800

    temp

---
+++ src/main/java/com/jarome/leetcode/test4/design/MinStack.java
@@ -0,0 +1,27 @@
+package com.jarome.leetcode.test4.design;
+
+/**
+ * 最小栈
+ */
+public class MinStack {
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        return 0;
+    }
+
+    public int getMin() {
+        return 0;
+    }
+}
--- src/main/java/com/jarome/leetcode/test4/design/UpsetNum.java
+++ src/main/java/com/jarome/leetcode/test4/design/UpsetNum.java
@@ -1,15 +1,17 @@
 package com.jarome.leetcode.test4.design;
 
+import java.util.Random;
+
 public class UpsetNum {
 
     class Solution {
 
         int[] nums;
-        int x = 0;
-        int y = 0;
+        Random random;
 
         public Solution(int[] nums) {
             this.nums = nums;
+            this.random = new Random();
         }
 
         public int[] reset() {
@@ -17,7 +19,24 @@ public class UpsetNum {
         }
 
         public int[] shuffle() {
+            if (this.nums == null) {
+                return null;
+            }
+            int[] newNums = nums.clone();
+            for (int i = 0; i < newNums.length; i++) {
+                int r = this.random.nextInt(i + 1);
+                swap(newNums, r, i);
+            }
             return nums;
         }
+
+        private void swap(int[] a, int i, int j) {
+            if (i != j) {
+                a[i] = a[i] + a[j];
+                a[j] = a[i] - a[j];
+                a[i] = a[i] - a[j];
+            }
+        }
+
     }
 }
