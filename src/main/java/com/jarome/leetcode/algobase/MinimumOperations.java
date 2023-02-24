aa5377bf87d288e507b2e9989faec9e03c77e539
jiangliuhong <ja_rome@163.com>
2023-02-24 18:20:37 +0800

    Create MinimumOperations.java

---
+++ src/main/java/com/jarome/leetcode/algobase/MinimumOperations.java
@@ -0,0 +1,15 @@
+package com.jarome.leetcode.algobase;
+
+/**
+ * @author jiangliuhong
+ * @date Created in 2023/2/24 18:20
+ *
+ * https://leetcode.cn/problems/make-array-zero-by-subtracting-equal-amounts/
+ */
+public class MinimumOperations {
+
+    public int minimumOperations(int[] nums) {
+
+    }
+
+}
